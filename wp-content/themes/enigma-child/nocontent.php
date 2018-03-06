51b99ebfc450be75abcdb8b4582c71a71d19b134
Riwalenn <riwalenn@gmail.com>
2018-03-06 17:42:59 +0100

    Articles
    articles seuls des pages catégories

---
+++ wp-content/themes/enigma-child/nocontent.php
@@ -0,0 +1,8 @@
+<div class="col-md-8">
+	<div class="error_404">
+		<h2><?php _e('404','enigma'); ?></h2>
+		<h4><?php _e('Whoops... Page non trouvée !!!','enigma'); ?></h4>
+		<p><?php _e('Nous sommes desolés mais la page que vous demandez n\'existe pas','enigma'); ?></p>
+		<p><a href="<?php echo home_url( '/' ); ?>"><button class="enigma_send_button" type="submit"><?php _e('Go To Homepage','enigma'); ?></button></a></p>
+	</div>
+</div>
