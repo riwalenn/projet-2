00fdfd90e4a275e79fb1ae36ccedf785a044c9cc
Riwalenn <riwalenn@gmail.com>
2018-03-07 23:36:37 +0100

    pages categories et articles

--- wp-content/themes/enigma-child/category.php
+++ wp-content/themes/enigma-child/category.php
@@ -4,7 +4,7 @@
 		<div class="row">
 		<?php if(have_posts()) :?>
 			<div class="col-md-12">
-			<h1><?php printf( __( 'Category Archives: %s', 'enigma' ), '<span>' . single_cat_title( '', false ) . '</span>' ); ?>
+			<h1><?php printf( __( 'Nos chalets disponibles en : %s', 'enigma' ), '<span>' . single_cat_title( '', false ) . '</span>' ); ?>
 			</h1>
 			</div>
 		<?php endif; ?>	
@@ -17,7 +17,7 @@
 	<?php 
 	if ( have_posts()): 
 	while ( have_posts() ): the_post();
-	get_template_part('post','content'); ?>	
+	get_template_part('post','content')?>	
 	<?php endwhile; 
 	endif; 
 	weblizar_navigation(); ?>
